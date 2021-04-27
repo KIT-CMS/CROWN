5aeb0ee1d0478fb805a378cc2a8c6616fd4c80f9
Sebastian Brommer <sebastian.brommer@kit.edu>
2021-04-27 17:56:38 +0200

    more clang

--- utility/utility.hxx
+++ utility/utility.hxx
@@ -1,20 +1,14 @@
-namespace utility{
-    bool ApproxEqual(auto value1, auto value2, double maxDelta=1e-5){
-    if (value1 == value2)
-        {
-            return true;
-        }
-        else
-        {
-            double delta = std::abs(value1 - value2);
-            if ((value1 + value2) != 0)
-            {
-                delta *= (2.0 / std::abs(value1 + value2));
-            }
-            return (delta < maxDelta);
-        }
+namespace utility {
+bool ApproxEqual(auto value1, auto value2, double maxDelta = 1e-5) {
+  if (value1 == value2) {
+    return true;
+  } else {
+    double delta = std::abs(value1 - value2);
+    if ((value1 + value2) != 0) {
+      delta *= (2.0 / std::abs(value1 + value2));
     }
-
+    return (delta < maxDelta);
+  }
+}
 
 } // end namespace utility
-
