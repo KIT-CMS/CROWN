3e526f005dc9e81850c4029c50b6fd24b5a6ef58
Sebastian Brommer <sebastian.brommer@kit.edu>
2021-04-27 17:44:11 +0200

    make quantities and lorentzvectors more flexible for shifts

--- lorentzvectors.hxx
+++ lorentzvectors.hxx
@@ -3,20 +3,17 @@
 
 namespace lorentzvectors{
 
-    auto buildparticle(auto df, const std::string& particle, const std::string& pairname, const int& position){
-        std::vector<std::string> cols;
-        cols.push_back(pairname);
-        cols.push_back(particle + "_pt");
-        cols.push_back(particle + "_eta");
-        cols.push_back(particle + "_phi");
-        cols.push_back(particle + "_mass");
-        const auto outputcol = "particle_" + std::to_string(position+1) + "_p4";
-        auto df1 = df.Define(outputcol,
+    auto buildparticle(auto df, const std::vector<std::string> quantities, const std::string outputname, const int& position){
+        // std::vector<std::string> cols;
+        // cols.push_back(particle + "_pt");
+        // cols.push_back(particle + "_eta");
+        // cols.push_back(particle + "_phi");
+        // cols.push_back(particle + "_mass");
+        // const auto outputcol = "p4_" + std::to_string(position+1);
+        auto df1 = df.Define(outputname,
             [position](const ROOT::RVec<int>& pair, const ROOT::RVec<float>& pts, const ROOT::RVec<float>& etas, const ROOT::RVec<float>& phis, const ROOT::RVec<float>& masses){
                 // the index of the particle is stored in the pair vector
                 const int index = pair[position];
-                // auto p4s = ROOT::VecOps::Construct<ROOT::Math::PtEtaPhiMVector>(pts, etas, phis, masses);
-                //return p4s[index];
                 Logger::get("lorentzvectors")->debug("pair {}", pair);
                 Logger::get("lorentzvectors")->debug("pts {}", pts);
                 Logger::get("lorentzvectors")->debug("etas {}", etas);
@@ -27,18 +24,32 @@ namespace lorentzvectors{
                 const auto p4 = ROOT::Math::PtEtaPhiMVector(pts[index], etas[index], phis[index], masses[index]);
                 Logger::get("lorentzvectors")->debug("P4 - Particle {} : {}", position, p4);
                 return p4;
-                }, cols);
+                }, quantities);
         return df1;
     }
 
     namespace mutau{
 
-        auto build(auto df, const std::string pairname)
+        auto build(auto df, const std::string& pairname, const std::vector<std::string>& muon_quantities, const std::vector<std::string>& tau_quantities,
+        const std::string& muon_p4_name, const std::string& tau_p4_name)
         {
             // first the muon
-            auto df1 = lorentzvectors::buildparticle(df, "Muon", pairname, 0);
+            // The quantities list should contain the paircolumn, and pt, eta, phi, mass
+            std::vector<std::string> muons;
+            muons.push_back(pairname);
+            for (auto i: muon_quantities)
+                muons.push_back(i);
+            for (auto i: muons)
+                Logger::get("lorentzvectors")->debug("Used muon quantities {}", i);
+            auto df1 = lorentzvectors::buildparticle(df, muons,muon_p4_name, 0);
             // now the tau
-            auto df2 = lorentzvectors::buildparticle(df1, "Tau", pairname, 1);
+            std::vector<std::string> taus;
+            taus.push_back(pairname);
+            for (auto i: tau_quantities)
+                taus.push_back(i);
+            for (auto i: taus)
+                Logger::get("lorentzvectors")->debug("Used tau quantities {}", i);
+            auto df2 = lorentzvectors::buildparticle(df1, taus,tau_p4_name, 1);
             return df2;
         }
     }
--- quantities.hxx
+++ quantities.hxx
@@ -2,29 +2,24 @@
 #include "utility/Logger.hxx"
 
 namespace quantities {
-    auto pt(auto df){
-        // for particle 1
-        auto df1 = df.Define("pt_1",[](const ROOT::Math::PtEtaPhiMVector& p4){ return p4.pt();},{"p4_1"});
-        // for particle 2
-        return df1.Define("pt_2",[](const ROOT::Math::PtEtaPhiMVector& p4){ return p4.pt();},{"p4_2"});
+    auto pt(auto df, std::vector<std::string> varSet, const std::string& outputname, const std::string& inputvector){
+        varSet.push_back(outputname);
+        return df.Define(outputname,[](const ROOT::Math::PtEtaPhiMVector& p4){ return p4.pt();},{inputvector});
     }
-    auto eta(auto df){
-        // for particle 1
-        auto df1 = df.Define("eta_1",[](const ROOT::Math::PtEtaPhiMVector& p4){ return p4.eta();},{"p4_1"});
-        // for particle 2
-        return df1.Define("eta_2",[](const ROOT::Math::PtEtaPhiMVector& p4){ return p4.eta();},{"p4_2"});
+    auto eta(auto df, std::vector<std::string> varSet, const std::string& outputname, const std::string& inputvector){
+        varSet.push_back(outputname);
+        return df.Define(outputname,[](const ROOT::Math::PtEtaPhiMVector& p4){ return p4.eta();},{inputvector});
     }
-    auto phi(auto df){
-        // for particle 1
-        auto df1 = df.Define("phi_1",[](const ROOT::Math::PtEtaPhiMVector& p4){ return p4.phi();},{"p4_1"});
-        // for particle 2
-        return df1.Define("phi_2",[](const ROOT::Math::PtEtaPhiMVector& p4){ return p4.phi();},{"p4_2"});
+    auto phi(auto df, std::vector<std::string> varSet, const std::string& outputname, const std::string& inputvector){
+        varSet.push_back(outputname);
+        return df.Define(outputname,[](const ROOT::Math::PtEtaPhiMVector& p4){ return p4.phi();},{inputvector});
     }
-    auto mvis(auto df){
+    auto m_vis(auto df, std::vector<std::string> varSet, const std::string& outputname, const std::string& inputvector1, const std::string& inputvector2){
+        varSet.push_back(outputname);
         // build visible mass from the two particles
         return df.Define("m_vis",[](const ROOT::Math::PtEtaPhiMVector& p4_1, const ROOT::Math::PtEtaPhiMVector& p4_2){
             auto const dileptonsystem = p4_1 + p4_2;
             return dileptonsystem.mass();
-        },{"p4_1", "p4_2"});
+        },{inputvector1, inputvector2});
     }
 } // end namespace quantities
