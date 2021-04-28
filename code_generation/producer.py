b4712567b506e732e51dbbf40c3ec007ca70245b
Sebastian Wozniewski <sebastian.wozniewski@kit.edu>
2021-04-28 16:47:49 +0200

    enable multiple outputs of python producers

--- code_generation/producer.py
+++ code_generation/producer.py
@@ -17,13 +17,17 @@ class Producer:
             q.children.append(self.output)
 
     def shift(self, name):
-        self.output.shift(
-            name
-        )  # crashes on purpose if output is None. This method should not be called for a producer without output
+        if isinstance(self.output, list):
+            for entry in self.output:
+                entry.shift(name)
+        else:
+            self.output.shift(
+                name
+            )  # crashes on purpose if output is None. This method should not be called for a producer without output
 
     def writecall(self, config, shift=""):
         config[shift]["output"] = (
-            "" if self.output == None else self.output.get_leaf(shift)
+            "" if self.output == None else '{"' + '","'.join([x.get_leaf(shift) for x in self.output]) + '"}' if isinstance(self.output, list) else self.output.get_leaf(shift)
         )
         config[shift]["input"] = ",".join([x.get_leaf(shift) for x in self.inputs])
         config[shift]["input_coll"] = (
@@ -37,15 +41,16 @@ class Producer:
     def writecalls(self, config):
         calls = [self.writecall(config)]
         if self.output != None:
-            for shift in self.output.shifts:
+            list_of_shifts = self.output[0].shifts if isinstance(self.output, list) else self.output.shifts #all entries must have same shifts
+            for shift in list_of_shifts:
                 calls.append(self.writecall(config, shift))
         return calls
 
 
 class VectorProducer(Producer):
-    def __init__(self, call, inputs, output, vec_config):
+    def __init__(self, call, inputs, output, vec_configs):
         super().__init__(call, inputs, output)
-        self.vec_config = vec_config
+        self.vec_configs = vec_configs
 
     def writecalls(self, config):
         basecall = self.call
@@ -54,8 +59,21 @@ class VectorProducer(Producer):
         if self.output != None:
             shifts.extend(self.output.shifts)
         for shift in shifts:
-            for val in config[shift][self.vec_config]:
-                self.call = basecall.format_map(SafeDict({self.vec_config: val}))
+            #check that all config lists (and output if applicable) have same length
+            l = len(config[shift][self.vec_configs[0]])
+            for key in self.vec_configs:
+                if l!=len(config[shift][key]):
+                    print("Following lists in config must have same length: %s, %s"%(self.vec_configs[0], key))
+                    raise Exception
+            if self.output!=None and len(self.output)!=l:
+                    print("VectorProducer expects either no output or same amount as entries in config lists (e.g. %s)!"%self.vec_configs[0])
+            for i in range(l):
+                helper_dict = {}
+                for key in self.vec_configs:
+                    helper_dict[key] = config[shift][key][i]
+                if self.output!=None:
+                    helper_dict["output"] = self.output[i]
+                self.call = basecall.format_map(SafeDict(helper_dict))
                 calls.append(self.writecall(config, shift))
         self.call = basecall
         return calls
@@ -79,5 +97,5 @@ MetFilter = VectorProducer(
     'metfilter::ApplyMetFilter({df}, "{met_filters}", "{met_filters}")',
     [],
     None,
-    "met_filters",
+    ["met_filters"],
 )
