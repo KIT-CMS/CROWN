71ccb9834fda917d32385755d6bbd0351ca7c623
Sebastian Wozniewski <sebastian.wozniewski@kit.edu>
2021-04-28 16:47:22 +0200

    add some comments and clean up

--- code_generation/code_generation.py
+++ code_generation/code_generation.py
@@ -2,8 +2,10 @@ import code_generation.producer as p
 
 
 def fill_template(t, config):
-    commandlist = ""
-    df_count = 0
+    commandlist = "" #string to be placed into code template
+    df_count = 0 #enumerate dataframes
+
+    #get commands of producers and append to the command list
     for producer in config["producers"]:
         for call in getattr(p, producer).writecalls(config):
             # print(call)
@@ -14,8 +16,4 @@ def fill_template(t, config):
             )
             df_count += 1
     commandlist += "  auto df_final = df%i;" % df_count
-    final = {}
-    final["CODE_GENERATION"] = commandlist
-    t = t.replace("  // {CODE_GENERATION}", commandlist)
-    # print(t)
-    return t
+    return t.replace("  // {CODE_GENERATION}", commandlist)
