fffcf3a9bd6829a5f66939e55f72a94941eba23b
Sebastian Brommer <sebastian.brommer@kit.edu>
2021-04-28 10:01:35 +0200

    using the correct clang format

--- analysis_template.cxx
+++ analysis_template.cxx
@@ -11,48 +11,50 @@
 static std::vector<std::string> varSet = {"run", "luminosityBlock", "event"};
 
 int main() {
-  TStopwatch timer;
-  timer.Start();
+    TStopwatch timer;
+    timer.Start();
 
-  // ROOT logging
-  auto verbosity = ROOT::Experimental::RLogScopedVerbosity(
-      ROOT::Detail::RDF::RDFLogChannel(), ROOT::Experimental::ELogLevel::kInfo);
+    // ROOT logging
+    auto verbosity = ROOT::Experimental::RLogScopedVerbosity(
+        ROOT::Detail::RDF::RDFLogChannel(),
+        ROOT::Experimental::ELogLevel::kInfo);
 
-  // file logging
-  ROOT::RDataFrame df0(
-      "Events",
-      "/work/sbrommer/ntuple_prototype/files/DYJetsToLL_SUmmer19_UL18.root");
-  // for testing, we limit to 1000 events only
-  // 1st stage: Good object selection
-  Logger::enableFileLogging("logs/main.txt");
-  Logger::setLevel(Logger::LogLevel::INFO);
-  Logger::get("main")->info("Starting Setup of Dataframe");
+    // file logging
+    ROOT::RDataFrame df0(
+        "Events",
+        "/work/sbrommer/ntuple_prototype/files/DYJetsToLL_SUmmer19_UL18.root");
+    // for testing, we limit to 1000 events only
+    // 1st stage: Good object selection
+    Logger::enableFileLogging("logs/main.txt");
+    Logger::setLevel(Logger::LogLevel::INFO);
+    Logger::get("main")->info("Starting Setup of Dataframe");
 
-  // auto df_final = df0;
+    // auto df_final = df0;
 
-  // {CODE_GENERATION}
+    // {CODE_GENERATION}
 
-  auto cutReport = df_final.Report();
+    auto cutReport = df_final.Report();
 
-  // Logger::get("main")->debug(df_final.Describe()); // <-- starting from
-  // ROOT 6.25
+    // Logger::get("main")->debug(df_final.Describe()); // <-- starting from
+    // ROOT 6.25
 
-  Logger::get("main")->info("Finished Setup");
-  Logger::get("main")->info("Runtime for setup (real time: {}, CPU time: {})",
-                            timer.RealTime(), timer.CpuTime());
-  timer.Continue();
+    Logger::get("main")->info("Finished Setup");
+    Logger::get("main")->info("Runtime for setup (real time: {}, CPU time: {})",
+                              timer.RealTime(), timer.CpuTime());
+    timer.Continue();
 
-  Logger::get("main")->info("Starting Evaluation");
-  df_final.Snapshot("ntuple", "test.root", varSet);
-  cutReport->Print();
-  Logger::get("main")->info("Finished Evaluation");
+    Logger::get("main")->info("Starting Evaluation");
+    df_final.Snapshot("ntuple", "test.root", varSet);
+    cutReport->Print();
+    Logger::get("main")->info("Finished Evaluation");
 
-  const auto nruns = df_final.GetNRuns();
-  if (nruns != 1) {
-    Logger::get("main")->critical("Analysis runs more than one event loop!");
-  }
+    const auto nruns = df_final.GetNRuns();
+    if (nruns != 1) {
+        Logger::get("main")->critical(
+            "Analysis runs more than one event loop!");
+    }
 
-  // as a first testcase, we work on selecting good muons
-  Logger::get("main")->info("Overall runtime (real time: {}, CPU time: {})",
-                            timer.RealTime(), timer.CpuTime());
+    // as a first testcase, we work on selecting good muons
+    Logger::get("main")->info("Overall runtime (real time: {}, CPU time: {})",
+                              timer.RealTime(), timer.CpuTime());
 }
--- metfilter.hxx
+++ metfilter.hxx
@@ -2,8 +2,8 @@ namespace metfilter {
 
 auto ApplyMetFilter(auto df, const std::string &flagname,
                     const std::string &filtername) {
-  return df.Filter([](const bool flag) { return flag; }, {flagname},
-                   filtername);
+    return df.Filter([](const bool flag) { return flag; }, {flagname},
+                     filtername);
 }
 
 } // namespace metfilter
