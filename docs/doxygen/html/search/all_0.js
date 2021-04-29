79e225130ad86d322b225f7d4396344b7c9a0509
Sebastian Brommer <sebastian.brommer@kit.edu>
2021-04-29 18:43:09 +0200

    remove doxygen output from repo

--- docs/doxygen/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['eta_0',['eta',['../namespacequantities.html#a88c8f32db1b0b21d2d9d6a0372c6ad20',1,'quantities']]]
-];
--- docs/doxygen/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['m_5fvis_1',['m_vis',['../namespacequantities.html#af7cd7e5effc0d3a2652dcbf13f32d7d4',1,'quantities']]]
-];
