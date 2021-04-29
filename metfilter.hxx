6d3216e8a8d8e9c8afdc67cbb1b8d69be3c15d80
Sebastian Brommer <sebastian.brommer@kit.edu>
2021-04-29 17:26:30 +0200

    add docstrings

--- metfilter.hxx
+++ metfilter.hxx
@@ -1,5 +1,14 @@
+/// The namespace that contains the metfilters
+
 namespace metfilter {
 
+/// Function to apply a metfilter to the dataframe
+///
+/// \param df the dataframe to add the quantity to
+/// \param flagname Name of the Filterflag in NanoAOD
+/// \param filtername Name of the Filter to be shown in the dataframe report
+///
+/// \returns a dataframe with the filter applied
 auto ApplyMetFilter(auto df, const std::string &flagname,
                     const std::string &filtername) {
     return df.Filter([](const bool flag) { return flag; }, {flagname},
