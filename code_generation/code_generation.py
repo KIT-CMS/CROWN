cfbeba8c16c9e00fe4815524f9a54126e0479814
Sebastian Wozniewski <sebastian.wozniewski@kit.edu>
2021-05-04 17:34:03 +0200

    update code_generation as well

--- code_generation/code_generation.py
+++ code_generation/code_generation.py
@@ -7,17 +7,36 @@ def fill_template(t, config):
     # generate list of commands
     commandlist = ""  # string to be placed into code template
     df_count = 0  # enumerate dataframes
+    df_scope_count = 0  # enumerate dataframes in certain scopes
 
     # get commands of producers and append to the command list
-    for producer in config["producers"]:
+    for producer in config["producers"]["global"]:
         commandlist += "\n    //" + producer.name + "\n"
-        for call in producer.writecalls(config):
-            # print(call)
+        for call in producer.writecalls(config, "global"):
             commandlist += (
                 "    auto df%i = " % (df_count + 1)
                 + call.format_map(SafeDict({"df": "df%i" % df_count}))
                 + ";\n"
             )
             df_count += 1
-    commandlist += "    auto df_final = df%i;" % df_count
+    for scope in config["producers"]:
+        if scope == "global":
+            continue
+        df_scope_count = 0
+        for producer in config["producers"][scope]:
+            commandlist += "\n    //" + producer.name + "\n"
+            for call in producer.writecalls(config, scope):
+                commandlist += (
+                    "    auto %s_df%i = " % (scope, df_scope_count + 1)
+                    + call.format_map(
+                        SafeDict(
+                            {"df": "df%i" % df_count}
+                            if df_scope_count == 0
+                            else {"df": "%s_df%i" % (scope, df_scope_count)}
+                        )
+                    )
+                    + ";\n"
+                )
+                df_scope_count += 1
+    commandlist += "    auto df_final = mt_df%i;" % df_scope_count
     return t.replace("    // {CODE_GENERATION}", commandlist)
