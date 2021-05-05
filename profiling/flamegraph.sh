3ae65a705a938e06e481ff76fc87748ee2a2a4b2
Sebastian Brommer <sebastian.brommer@kit.edu>
2021-05-05 17:59:54 +0200

    update flamegraph and debug output

--- profiling/flamegraph.sh
+++ profiling/flamegraph.sh
@@ -5,15 +5,15 @@ INPUTFILE=$2
 OUTPUTFILE=$3
 
 # Record samples with perf
-perf record -g $EXECUTABLE $INPUTFILE $OUTPUTFILE
-
+# perf record -g $EXECUTABLE
+perf record --call-graph dwarf $EXECUTABLE $INPUTFILE $OUTPUTFILE
 # Convert the data to be readable for flamegraphs
 perf script > out.perf
 
 # Perform the stack collapse
-curl -Os https://raw.githubusercontent.com/brendangregg/FlameGraph/master/stackcollapse-perf.pl > stackcollapse-perf.pl
+curl -Os https://raw.githubusercontent.com/eguiraud/FlameGraph/160b531f4c5ef0fec37e2b719ec609842a02aa99/stackcollapse-perf.pl > stackcollapse-perf.pl
 perl stackcollapse-perf.pl out.perf > out.folded
 
 # Generate the flamegraph
-curl -Os https://raw.githubusercontent.com/brendangregg/FlameGraph/master/flamegraph.pl > flamegraph.pl
+curl -Os https://raw.githubusercontent.com/brendangregg/FlameGraph/a258e78f17abdf2ce21c2515cfe8306a44774e2a/flamegraph.pl > flamegraph.pl
 perl flamegraph.pl out.folded > flamegraph.svg
