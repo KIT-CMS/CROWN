4747b5e446d3669355c8060aa2d7cc6168015b62
Sebastian Brommer <sebastian.brommer@kit.edu>
2021-05-06 12:09:36 +0200

    style..

--- utility/utility.hxx
+++ utility/utility.hxx
@@ -15,16 +15,4 @@ bool ApproxEqual(auto value1, auto value2, double maxDelta = 1e-5) {
         return (delta < maxDelta);
     }
 }
-
-template <std::size_t ...Idxs>
-auto helper(std::index_sequence<Idxs...>) {
-    return [] (decltype(Idxs, 0)...masks) { return (masks *...); };
-}
-
-template <std::size_t N>
-auto MaskCombiner() {
-    return helper(std::make_index_sequence<N>{});
-};
-
-
 } // end namespace utility
