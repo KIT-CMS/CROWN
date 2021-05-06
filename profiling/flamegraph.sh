47bfe3e189d998f1de6c818b83f0266a8004eeeb
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2021-05-06 14:41:43 +0200

    update profiling script

--- profiling/flamegraph.sh
+++ profiling/flamegraph.sh
@@ -5,15 +5,17 @@ INPUTFILE=$2
 OUTPUTFILE=$3
 
 # Record samples with perf
-perf record -g $EXECUTABLE $INPUTFILE $OUTPUTFILE
+perf record --call-graph dwarf $EXECUTABLE $INPUTFILE $OUTPUTFILE
 
 # Convert the data to be readable for flamegraphs
 perf script > out.perf
 
 # Perform the stack collapse
-curl -Os https://raw.githubusercontent.com/brendangregg/FlameGraph/master/stackcollapse-perf.pl > stackcollapse-perf.pl
+# The repo of Enrico (eguiraud) holds a small fix printing long templates in a nicer way.
+BASE_URL=https://raw.githubusercontent.com/eguiraud/FlameGraph/dc043011dd74c9bdda9a9ec4254a17e45b76f560
+curl -Os ${BASE_URL}/stackcollapse-perf.pl > stackcollapse-perf.pl
 perl stackcollapse-perf.pl out.perf > out.folded
 
 # Generate the flamegraph
-curl -Os https://raw.githubusercontent.com/brendangregg/FlameGraph/master/flamegraph.pl > flamegraph.pl
+curl -Os ${BASE_URL}/flamegraph.pl > flamegraph.pl
 perl flamegraph.pl out.folded > flamegraph.svg
