b1b53c385461e2f514a079421d227edd6f3a6595
Sebastian Wozniewski <sebastian.wozniewski@kit.edu>
2021-06-01 11:03:12 +0200

    add option to ignore variations for given producers

--- config/utility.py
+++ config/utility.py
@@ -1,10 +1,14 @@
 import copy
 
 
-def AddSystematicShift(config, name, change_dict, base_producers):
+def AddSystematicShift(
+    config, name, change_dict, base_producers, sanetize_producers=[], scope="global"
+):
     name = "__" + name
     shift_config = copy.deepcopy(config[""])
     shift_config.update(change_dict)
     config[name] = shift_config
+    for producer in sanetize_producers:
+        producer[0].ignore_shift(name, producer[1])
     for producer in base_producers:
-        producer.shift(name)
+        producer.shift(name, scope)
