445b57d29eb197a56fd24ab9482fb6753f8e98ed
Sebastian Wozniewski <sebastian.wozniewski@kit.edu>
2021-06-11 15:47:51 +0200

    reorganize scope treatment in config and inputs

--- config/utility.py
+++ config/utility.py
@@ -2,13 +2,14 @@ import copy
 
 
 def AddSystematicShift(
-    config, name, change_dict, base_producers, sanetize_producers=[], scope="global"
+    config, name, change_dict, base_producers, sanetize_producers=[]
 ):
     name = "__" + name
     shift_config = copy.deepcopy(config[""])
-    shift_config.update(change_dict)
+    for scope in change_dict:
+        shift_config[scope].update(change_dict[scope])
     config[name] = shift_config
     for producer in sanetize_producers:
         producer[0].ignore_shift(name, producer[1])
     for producer in base_producers:
-        producer.shift(name, scope)
+        producer[0].shift(name, producer[1])
