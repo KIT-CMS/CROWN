ac6ba76bad29db1678d6ada9f86a1d1259c50d3a
Sebastian Brommer <sebastian.brommer@kit.edu>
2021-07-20 11:05:54 +0200

    final fix of reserve_scopes logic

--- code_generation/quantity.py
+++ code_generation/quantity.py
@@ -26,9 +26,8 @@ class Quantity:
         This check is triggered for every Producer.
         """
         log.debug("Checking {} / scope {}".format(self.name, scope))
-        if (scope != "global" and self.defined_for_scopes == []) or (
-            scope not in self.defined_for_scopes
-            and "global" not in self.defined_for_scopes
+        if (scope == "global" and self.defined_for_scopes == []) or (
+            scope != "global" and scope not in self.defined_for_scopes
         ):
             self.defined_for_scopes.append(scope)
         else:
