2b34c7ca3aaa7e88b9a71099ea9e08d54b8555bc
Sebastian Wozniewski <sebastian.wozniewski@kit.edu>
2021-08-24 11:18:36 +0200

    treatment of command line arguments

--- generate.py
+++ generate.py
@@ -14,14 +14,26 @@ parser.add_argument("--analysis", type=str, help="Name of the analysis config")
 parser.add_argument(
     "--channels",
     type=str,
-    nargs="*",
+    nargs="+",
     help='Channels to be activated. To select all, choose "auto"',
 )
 parser.add_argument(
-    "--shifts", type=str, help='Shifts to be activated. To select all, choose "auto"'
+    "--shifts",
+    type=str,
+    nargs="*",
+    help='Shifts to be activated. To select all, choose "auto"',
 )
 parser.add_argument(
-    "--samples", type=str, help='Samples to be processed. To select all, choose "auto"'
+    "--samples",
+    type=str,
+    nargs="+",
+    help='Samples to be processed. To select all, choose "auto"',
+)
+parser.add_argument(
+    "--eras",
+    type=str,
+    nargs="+",
+    help='Eras to be processed. To select all, choose "auto"',
 )
 parser.add_argument("--debug", type=str, help='set debug mode for building"')
 args = parser.parse_args()
@@ -29,18 +41,20 @@ args = parser.parse_args()
 # ggH
 # vbf
 # remaining htt and hww?
-# embedded
+# emb
 # tt
 # vv and single top
 # DY
 # WJets
 # data
+if "auto" in args.samples:
+    args.samples = ["ggh", "vbf", "rem_htt", "emb", "tt", "vv", "dy", "wj", "data"]
+if "auto" in args.eras:
+    args.eras = ["2016", "2017", "2018"]
 
 executables = []
-eras = ["2018"]
-sample_groups = ["emb"]
-for era in eras:
-    for sample_group in sample_groups:
+for era in args.eras:
+    for sample_group in args.samples:
         ## setup logging
         if not path.exists("generation_logs"):
             makedirs("generation_logs")
@@ -62,7 +76,7 @@ for era in eras:
         analysis = importlib.import_module("config." + args.analysis)
         root.info("Generating code for {}...".format(sample_group))
         root.info("Configuration used: {}".format(analysis))
-        config = analysis.build_config(era, sample_group)
+        config = analysis.build_config(era, sample_group, args.channels, args.shifts)
         # fill code template and write executable
         with open(args.template, "r") as template_file:
             template = template_file.read()
