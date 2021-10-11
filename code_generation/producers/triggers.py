c24bb8aa2c8298e09bd9778ea7be8290297c2368
Sebastian Brommer <sebastian.brommer@kit.edu>
2021-10-11 15:15:33 +0200

    expand producers for multiple channels

--- code_generation/producers/triggers.py
+++ code_generation/producers/triggers.py
@@ -6,8 +6,23 @@ from code_generation.producer import TriggerVectorProducer
 # Set of producers used for trigger flags
 ####################
 
-GenerateSingleMuonTriggerFlags = TriggerVectorProducer(
-    name="GenerateSingleMuonTriggerFlags",
+MMGenerateSingleMuonTriggerFlags = TriggerVectorProducer(
+    name="MMGenerateSingleMuonTriggerFlags",
+    call='trigger::GenerateSingleTriggerFlag({df}, {output}, {input}, "{hlt_path}", {ptcut}, {etacut}, {trigger_particle_id}, {filterbit}, {max_deltaR_triggermatch} )',
+    input=[
+        q.p4_1,
+        nanoAOD.TriggerObject_bit,
+        nanoAOD.TriggerObject_id,
+        nanoAOD.TriggerObject_pt,
+        nanoAOD.TriggerObject_eta,
+        nanoAOD.TriggerObject_phi,
+    ],
+    output="flagname",
+    scope=["mm"],
+    vec_config="singlemoun_trigger",
+)
+MTGenerateSingleMuonTriggerFlags = TriggerVectorProducer(
+    name="MTGenerateSingleMuonTriggerFlags",
     call='trigger::GenerateSingleTriggerFlag({df}, {output}, {input}, "{hlt_path}", {ptcut}, {etacut}, {trigger_particle_id}, {filterbit}, {max_deltaR_triggermatch} )',
     input=[
         q.p4_1,
@@ -21,7 +36,22 @@ GenerateSingleMuonTriggerFlags = TriggerVectorProducer(
     scope=["mt"],
     vec_config="singlemoun_trigger",
 )
-GenerateCrossTriggerFlags = TriggerVectorProducer(
+EMGenerateSingleMuonTriggerFlags = TriggerVectorProducer(
+    name="EMGenerateSingleMuonTriggerFlags",
+    call='trigger::GenerateSingleTriggerFlag({df}, {output}, {input}, "{hlt_path}", {ptcut}, {etacut}, {trigger_particle_id}, {filterbit}, {max_deltaR_triggermatch} )',
+    input=[
+        q.p4_2,
+        nanoAOD.TriggerObject_bit,
+        nanoAOD.TriggerObject_id,
+        nanoAOD.TriggerObject_pt,
+        nanoAOD.TriggerObject_eta,
+        nanoAOD.TriggerObject_phi,
+    ],
+    output="flagname",
+    scope=["em"],
+    vec_config="singlemoun_trigger",
+)
+MTGenerateCrossTriggerFlags = TriggerVectorProducer(
     name="GenerateCrossTriggerFlags",
     call='trigger::GenerateDoubleTriggerFlag({df}, {output}, {input}, "{hlt_path}", {p1_ptcut}, {p2_ptcut}, {p1_etacut}, {p2_etacut}, {p1_trigger_particle_id}, {p2_trigger_particle_id}, {p1_filterbit}, {p2_filterbit}, {max_deltaR_triggermatch})',
     input=[
