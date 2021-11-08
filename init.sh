94ea3f81b073f11fe6222a4432044647d0bf6a2b
Sebastian Brommer <sebastian.brommer@kit.edu>
2021-11-08 16:47:25 +0100

    add check for centos7

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
-# source /cvmfs/sft.cern.ch/lcg/views/LCG_99/x86_64-centos7-clang10-opt/setup.sh
-source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc9-opt/setup.sh
-# source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc9-dbg/setup.sh
+if uname -a | grep -E 'el7' -q
+then
+    # source /cvmfs/sft.cern.ch/lcg/views/LCG_99/x86_64-centos7-clang10-opt/setup.sh
+    source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc9-opt/setup.sh
+    # source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc9-dbg/setup.sh
+else
+    echo "You are not running on CentOS7, things will propably break..."
+fi
