fedb10d076b9035e5593dcea3bd3015e99b4c6bb
Enrico Guiraud <enrico.guiraud@cern.ch>
2022-04-19 16:42:35 +0200

    Mark `default_lorentzvector` global `const`
    This should avoid ODR issues as the const-ness also gives the
    variable internal linkage.

--- src/defaults.hxx
+++ src/defaults.hxx
@@ -8,7 +8,7 @@ const int default_int = -10;
 const int default_pdgid = -999;
 const float default_float = -10.0;
 const UChar_t default_uchar = -10;
-auto default_lorentzvector = ROOT::Math::PtEtaPhiMVector(
+const auto default_lorentzvector = ROOT::Math::PtEtaPhiMVector(
     default_float, default_float, default_float, default_float);
 
 #endif /* GUARDDEFAULTS_H */
