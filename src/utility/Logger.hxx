bc08e9038a05353a50b4b66314f8596b21c24e0b
Enrico Guiraud <enrico.guiraud@cern.ch>
2022-04-19 16:21:58 +0200

    Include system headers as `<header>`, not `"header"`
    This helps tools like clang-tidy parse this header correctly.

--- src/utility/Logger.hxx
+++ src/utility/Logger.hxx
@@ -1,10 +1,10 @@
 #ifndef GUARDLOGGER_H
 #define GUARDLOGGER_H
 
-#include "spdlog/fmt/ostr.h" // for formatting of RVecs
-#include "spdlog/sinks/basic_file_sink.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
-#include "spdlog/spdlog.h"
+#include <spdlog/fmt/ostr.h> // for formatting of RVecs
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
 
 class Logger {
   public:
