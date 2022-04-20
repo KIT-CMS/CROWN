9c4f80d0e3a19e38faafe4c0d47bd1191dd62486
Sebastian Brommer <sebastian.brommer@kit.edu>
2022-04-20 19:42:42 +0200

    move lambda basefunctions to inline definitions

--- include/basefunctions.hxx
+++ include/basefunctions.hxx
@@ -32,15 +32,138 @@ template <typename T>
 ROOT::RDF::RNode FilterIntSelection(auto &df, const std::string &quantity,
                                     const std::vector<T> &selection,
                                     const std::string &filtername);
-auto FilterMax(const float &cut);
-auto FilterAbsMax(const float &cut);
-auto FilterMin(const float &cut);
-auto FilterMinInt(const int &cut);
-auto FilterAbsMin(const float &cut);
-auto MultiplyTwoMasks();
-auto FilterID(const int &index);
-auto FilterJetID(const int &index);
-auto FilterJetPUID(const int &PUindex, const float &PUptcut);
+/// Function to apply a maximal filter requirement to a quantity.
+/// Returns true if the value is smaller than the given cut value
+///
+/// \param cut The cut value of the filter
+///
+/// \returns a lambda function to be used in RDF Define
+inline auto FilterMax(const float &cut) {
+    return [cut](const ROOT::RVec<float> &values) {
+        ROOT::RVec<int> mask = values < cut;
+        return mask;
+    };
+}
+
+/// Function to apply a maximal filter requirement to a quantity.
+/// Returns true if the absolute value is smaller than the given cut value
+///
+/// \param cut The cut value of the filter
+///
+/// \returns a lambda function to be used in RDF Define
+inline auto FilterAbsMax(const float &cut) {
+    return [cut](const ROOT::RVec<float> &values) {
+        ROOT::RVec<int> mask = abs(values) < cut;
+        return mask;
+    };
+}
+
+/// Function to apply a minimal filter requirement to a quantity.
+/// Returns true if the value is larger than the given cut value
+///
+/// \param cut The cut value of the filter
+///
+/// \returns a lambda function to be used in RDF Define
+inline auto FilterMin(const float &cut) {
+    // As in ROOT, for min we use >=
+    return [cut](const ROOT::RVec<float> &values) {
+        ROOT::RVec<int> mask = values >= cut;
+        return mask;
+    };
+}
+
+/// Function to apply a minimal filter requirement to an integer quantity.
+/// Returns true if the value is larger than the given cut value
+///
+/// \param cut The cut value of the filter
+///
+/// \returns a lambda function to be used in RDF Define
+inline auto FilterMinInt(const int &cut) {
+    // As in ROOT, for min we use >=
+    return [cut](const ROOT::RVec<int> &values) {
+        ROOT::RVec<int> mask = values >= cut;
+        return mask;
+    };
+}
+
+/// Function to apply a minimal filter requirement to a quantity.
+/// Returns true if the absolute value is larger than the given cut value
+///
+/// \param cut The cut value of the filter
+///
+/// \returns a lambda function to be used in RDF Define
+inline auto FilterAbsMin(const float &cut) {
+    return [cut](const ROOT::RVec<float> &values) {
+        ROOT::RVec<int> mask = abs(values) >= cut;
+        return mask;
+    };
+}
+
+/// Function to combine two RVec Masks by multiplying the two RVec elementwise
+///
+/// \param mask_1 The first mask
+/// \param mask_2 The second mask
+///
+/// \returns a lambda function which returns the multiplication of the two masks
+inline auto MultiplyTwoMasks() {
+    return [](const ROOT::RVec<Int_t> &mask_1,
+              const ROOT::RVec<Int_t> &mask_2) { return mask_1 * mask_2; };
+}
+
+/// Function to filter the Tau ID in NanoAOD. The disciminator output is stored
+/// as a bitmask. In order to filter based on a given WP, a bitwise comparison
+/// between the given ID and the filtered index is performed. Return true if the
+/// working point is passed by a tau.
+///
+/// \param index The bitmask index to be used for comparison
+///
+/// \returns a lambda function to be used in RDF Define
+inline auto FilterID(const int &index) {
+    return [index](const ROOT::RVec<UChar_t> &IDs) {
+        ROOT::RVec<int> mask;
+        for (auto const ID : IDs) {
+            if (index > 0)
+                mask.push_back(std::min(1, int(ID & 1 << index - 1)));
+            else
+                mask.push_back(int(1));
+        }
+        return mask;
+    };
+}
+/// Function to filter the Jet ID in NanoAOD. Ths values are stored bitwise, so
+/// the integer value has to be decoded into binary, and then the value of the
+/// index bit has to be compared.
+///
+/// \param index The bitmask index to be used for comparison
+///
+/// \returns a lambda function to be used in RDF Define
+inline auto FilterJetID(const int &index) {
+    return [index](const ROOT::RVec<Int_t> &IDs) {
+        ROOT::RVec<int> mask;
+        for (auto const ID : IDs) {
+            mask.push_back(std::min(1, (ID >> index) & 1));
+        }
+        return mask;
+    };
+}
+/// Function to filter the Jet pileup ID in NanoAOD. This ID is applied on jets
+/// below a given pt threshold. The jet pileup ID has 4 possible values:
+/// 0==fail, 4==pass loose, 6==pass loose and medium, 7==pass loose, medium and
+/// tight
+///
+/// \param PUindex The index to be used for comparison
+/// \param PUptcut The pt threshold to be used for comparison
+///
+/// \returns a lambda function to be used in RDF Define
+inline auto FilterJetPUID(const int &PUindex, const float &PUptcut) {
+    return [PUindex, PUptcut](const ROOT::RVec<Int_t> &PUIDs,
+                              const ROOT::RVec<float> &jet_pts) {
+        ROOT::RVec<int> tmp_mask1 = PUIDs >= PUindex;
+        ROOT::RVec<int> tmp_mask2 = jet_pts >= PUptcut;
+        ROOT::RVec<int> mask = (tmp_mask1 + tmp_mask2) > 0;
+        return mask;
+    };
+}
 template <class... Inputs>
 ROOT::RDF::RNode
 evaluateWorkspaceFunction(auto &df, const std::string &outputname,
