f44b4e3fbc5ca6fd563d9ba1003bc6731524b9dd
Sebastian Brommer <sebastian.brommer@kit.edu>
2022-05-03 14:10:06 +0200

    fix NNLO typo

--- include/htxs.hxx
+++ include/htxs.hxx
@@ -2,17 +2,18 @@
 #define GUARDHTXS_H
 
 namespace htxs {
-ROOT::RDF::RNode ggHNLLOWeights(ROOT::RDF::RNode df, const std::string &weight_name,
-                                const std::string &rootfilename,
-                                const std::string &generator,
-                                const std::string &htxs_pth,
-                                const std::string &htxs_njets);
 ROOT::RDF::RNode
-ggH_WG1_uncertainties(ROOT::RDF::RNode df, const std::vector<std::string> &weight_names,
+ggHNNLOWeights(ROOT::RDF::RNode df, const std::string &weight_name,
+               const std::string &rootfilename, const std::string &generator,
+               const std::string &htxs_pth, const std::string &htxs_njets);
+ROOT::RDF::RNode
+ggH_WG1_uncertainties(ROOT::RDF::RNode df,
+                      const std::vector<std::string> &weight_names,
                       const std::string &htxs_flag, const std::string &htxs_pth,
                       const std::string &htxs_njets);
 ROOT::RDF::RNode
-qqH_WG1_uncertainties(ROOT::RDF::RNode df, const std::vector<std::string> &weight_names,
+qqH_WG1_uncertainties(ROOT::RDF::RNode df,
+                      const std::vector<std::string> &weight_names,
                       const std::string &htxs_flag, const size_t &idx = 0);
 } // namespace htxs
 #endif /* GUARDHTXS_H */
