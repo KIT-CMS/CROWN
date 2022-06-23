af75976f1658d5ef18248714b68c8e2538071b8c
Sebastian Brommer <sebastian.brommer@kit.edu>
2022-06-23 12:25:00 +0200

    add black CI check

---
+++ scripts/python-formatting.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FOUND_ISSUE=0
+
+for FILENAME in $(find . -name "*.py");
+do
+    black --check $FILENAME
+    RETURN_VALUE=$?
+    if [ $RETURN_VALUE -ne 0 ]
+    then
+        black --diff $FILENAME 2> /dev/null
+        FOUND_ISSUE=1
+    fi
+done
+
+exit $FOUND_ISSUE
