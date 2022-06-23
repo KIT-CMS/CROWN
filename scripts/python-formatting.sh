5f07a5034f094dc22b482aa758601ef811f18d1c
Sebastian Brommer <sebastian.brommer@kit.edu>
2022-06-23 12:37:17 +0200

    update to CI script to exclude law

--- scripts/python-formatting.sh
+++ scripts/python-formatting.sh
@@ -2,14 +2,18 @@
 
 FOUND_ISSUE=0
 
-for FILENAME in $(find . -name "*.py");
+for FILENAME in $(find . -path ./law -prune -o -name "*.py");
 do
-    black --check $FILENAME
-    RETURN_VALUE=$?
-    if [ $RETURN_VALUE -ne 0 ]
-    then
-        black --diff $FILENAME 2> /dev/null
-        FOUND_ISSUE=1
+    # only run the check if the filename ends with .py
+    if [[ $FILENAME == *.py ]]; then
+        echo "Checking $FILENAME"
+        black --check $FILENAME
+        RETURN_VALUE=$?
+        if [ $RETURN_VALUE -ne 0 ]
+        then
+            black --diff $FILENAME 2> /dev/null
+            FOUND_ISSUE=1
+        fi
     fi
 done
 
