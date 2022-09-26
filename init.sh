071d5ca7ebbbddffbe46fdb2bdd1d9eb02b8812e
zhiyuanlcern <zhiyuan.li@cern.ch>
2022-09-26 15:16:13 +0800

    adding tau analysis, delete unittest, some modification to git address for PKU usage

--- init.sh
+++ init.sh
@@ -35,7 +35,7 @@ else
     elif [[ "$1" == "earlyrun3" && ! -d "${SCRIPT_DIR}/analysis_configurations/earlyrun3" ]]
     then
         echo "Cloning analysis earlyrun3 into ${SCRIPT_DIR}/analysis_configurations/earlyrun3"
-        git clone https://github.com/khaosmos93/CROWN-config-earlyRun3.git ${SCRIPT_DIR}/analysis_configurations/earlyrun3
+        git clone git@github.com:khaosmos93/CROWN-config-earlyRun3.git ${SCRIPT_DIR}/analysis_configurations/earlyrun3
     elif [[ "$1" == "whtautau" && ! -d "${SCRIPT_DIR}/analysis_configurations/whtautau" ]]
     then
         echo "Cloning analysis whtautau into ${SCRIPT_DIR}/analysis_configurations/whtautau"
