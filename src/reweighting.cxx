56ae6e968bd9e131b2a558b6fa6b8586b95c81fc
Sebastian Brommer <sebastian.brommer@kit.edu>
2023-01-09 13:38:59 +0100

    allow the use of modifiers within Shift configs (#186)

--- src/reweighting.cxx
+++ src/reweighting.cxx
@@ -180,5 +180,68 @@ ROOT::RDF::RNode zPtMassReweighting(ROOT::RDF::RNode df,
         gen_boson + "_pt");
     return df3;
 }
+
+/**
+ * @brief Function used to evaluate the lheScaleweight of an event. The weights
+are stored in the nanoAOD file an defined as w_var/w_nominal. Depending on the
+selected muR and muF value, a specific index has to be returned. The mapping
+between the index and the muR and muF values is:
+
+muF        | MuR   | index
+------------|-------|-------
+ 0.5        | 0.5   | 0
+ 1.0        | 0.5   | 1
+ 2.0        | 0.5   | 2
+ 0.5        | 1.0   | 3
+ 1.0        | 1.0   | 4
+ 2.0        | 1.0   | 5
+ 0.5        | 2.0   | 6
+ 1.0        | 2.0   | 7
+ 2.0        | 2.0   | 8
+
+
+ *
+ * @param df The input dataframe
+ * @param weightname the output name of the generated weight
+ * @param lhe_scale_weights name of the column containing the lhe scale weights
+ * @param muR the value of muR, possible values are 0.5, 1.0, 2.0
+ * @param muF the value of muF, possible values are 0.5, 1.0, 2.0
+ * @return ROOT::RDF::RNode
+ */
+
+ROOT::RDF::RNode lhe_scale_weights(ROOT::RDF::RNode df,
+                                   const std::string &weightname,
+                                   const std::string &lhe_scale_weights,
+                                   const float muR, const float muF) {
+    // find the index we have to use, first check if the muR and muF values are
+    // valid, only 0.5, 1.0, 2.0 are allowed
+    std::vector<float> allowed_values = {0.5, 1.0, 2.0};
+    if (std::find(allowed_values.begin(), allowed_values.end(), muR) ==
+        allowed_values.end()) {
+        Logger::get("lhe_scale_weights")
+            ->error("Invalid value for muR: {}}", muR);
+        throw std::runtime_error("Invalid value for muR");
+    }
+    if (std::find(allowed_values.begin(), allowed_values.end(), muF) ==
+        allowed_values.end()) {
+        Logger::get("lhe_scale_weights")
+            ->error("Invalid value for muF: {}}", muF);
+        throw std::runtime_error("Invalid value for muF");
+    }
+    // now find the index
+    std::map<std::pair<const float, const float>, int> index_map = {
+        {{0.5, 0.5}, 0}, {{1.0, 0.5}, 1}, {{2.0, 0.5}, 2},
+        {{0.5, 1.0}, 3}, {{1.0, 1.0}, 4}, {{2.0, 1.0}, 5},
+        {{0.5, 2.0}, 6}, {{1.0, 2.0}, 7}, {{2.0, 2.0}, 8}};
+    std::pair<const float, const float> variations = {muR, muF};
+    int index = index_map[variations];
+    auto lhe_scale_weights_lambda =
+        [index](const ROOT::RVec<float> scale_weight) {
+            return scale_weight.at(index);
+        };
+    auto df1 =
+        df.Define(weightname, lhe_scale_weights_lambda, {lhe_scale_weights});
+    return df1;
+}
 } // namespace reweighting
 #endif /* GUARD_REWEIGHTING_H */
