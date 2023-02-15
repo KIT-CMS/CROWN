abc26fef60dcd777a685af39d1e328286824c743
Sebastian Brommer <sebastian.brommer@kit.edu>
2023-02-15 18:40:34 +0100

    add functionality to go from tarball to the exact configuration + CROWN setup

---
+++ checks/setup_from_tarball.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+tarball=$1
+checkout_location=$2
+# check if tarball is provided
+if [ -z "$tarball" ]; then
+    echo "ERROR: path to a tarball or an unpacked folder must be provided"
+    exit 1
+fi
+# create the checkout location if it does not exist
+if [ ! -d "$checkout_location" ]; then
+    mkdir -p $checkout_location
+fi
+
+if [ -d "$tarball" ]; then
+    mkdir temp
+    cp -r $tarball temp/
+    tarball=$(realpath temp/$tarball)
+fi
+# if tarball is a file, unpack it here
+if [ -f "$tarball" ]; then
+    mkdir temp
+    tar -xzf $tarball -C temp/
+    tarball=$(realpath temp/$tarball)
+fi
+# go to the checkout location
+cd $checkout_location
+echo "Using temporary tarball copy in $tarball"
+# now read the commit hashes from the diff folder in the tarball
+base_commit=$(cat $tarball/diff/base_commit_hash.txt)
+analysis_commit=$(cat $tarball/diff/analysis_commit_hash.txt)
+analysis_name=$(cat $tarball/diff/analysis_name.txt)
+
+# now checkout the base repository with the commit hash
+git clone --recursive git@github.com:KIT-CMS/CROWN
+cd CROWN
+git checkout $base_commit
+# now apply the base diff
+git apply $tarball/diff/base_diff.patch
+# now setup the analysis with the init script
+bash init.sh $analysis_name
+# now checkout the analysis repository with the commit hash
+cd analysis_configurations/$analysis_name
+git checkout $analysis_commit
+# now apply the analysis diff
+git apply $tarball/diff/analysis_diff.patch
+# cleanup the temporary tarball
+rm -rf $tarball
+echo "Setup from tarball finished. You can now run the analysis."
