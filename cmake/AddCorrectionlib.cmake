5cae00b9b4589c759cce424d6fe0515624a750f8
Sebastian Brommer <sebastian.brommer@kit.edu>
2024-02-29 15:44:30 +0100

    format cmake files

--- cmake/AddCorrectionlib.cmake
+++ cmake/AddCorrectionlib.cmake
@@ -1,34 +1,39 @@
 function(install_correctionlib)
-    execute_process(COMMAND "${Python_EXECUTABLE}" "-c" "import correctionlib; print(correctionlib.__version__)"
-                    RESULT_VARIABLE PACKAGE_NOT_FOUND
-                    OUTPUT_VARIABLE PACKAGE_VERSION
-                    OUTPUT_STRIP_TRAILING_WHITESPACE)
-    if(${PACKAGE_NOT_FOUND} EQUAL 1)
-        execute_process(COMMAND ${Python_EXECUTABLE} -m pip install ${_pip_args} git+https://github.com/cms-nanoAOD/correctionlib.git)
-    endif()
-    message(STATUS "Found correctionlib !")
+  execute_process(
+    COMMAND "${Python_EXECUTABLE}" "-c"
+            "import correctionlib; print(correctionlib.__version__)"
+    RESULT_VARIABLE PACKAGE_NOT_FOUND
+    OUTPUT_VARIABLE PACKAGE_VERSION
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  if(${PACKAGE_NOT_FOUND} EQUAL 1)
+    execute_process(
+      COMMAND ${Python_EXECUTABLE} -m pip install ${_pip_args}
+              git+https://github.com/cms-nanoAOD/correctionlib.git)
+  endif()
+  message(STATUS "Found correctionlib !")
 endfunction()
 
-# Adding correctionlib for scale factor evaluation
-# for now the official pip package has some problem
-# in the future "find_python_package(correctionlib correctionlib X.X)" should hopefully work
+# Adding correctionlib for scale factor evaluation for now the official pip
+# package has some problem in the future "find_python_package(correctionlib
+# correctionlib X.X)" should hopefully work
 install_correctionlib()
 message(STATUS "Setting up correctionlib ...")
-execute_process(COMMAND correction config --cmake
-    OUTPUT_VARIABLE CORRECTION_LIB_ARGS
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(
+  COMMAND correction config --cmake
+  OUTPUT_VARIABLE CORRECTION_LIB_ARGS
+  OUTPUT_STRIP_TRAILING_WHITESPACE)
 string(REPLACE -Dcorrectionlib_DIR= "" CORRECTIONLIBPATH ${CORRECTION_LIB_ARGS})
 # if correctionlib comes from cvmfs, change the correctionlibpath accordingly
-if (${CORRECTIONLIBPATH} MATCHES "^/cvmfs/")
-    message(STATUS "Setting up correctionlib from cvmfs ...")
-    set(USING_CVMFS TRUE)
-    find_package(correctionlib)
-    find_library(CORRECTION_LIB_PATH correctionlib)
+if(${CORRECTIONLIBPATH} MATCHES "^/cvmfs/")
+  message(STATUS "Setting up correctionlib from cvmfs ...")
+  set(USING_CVMFS TRUE)
+  find_package(correctionlib)
+  find_library(CORRECTION_LIB_PATH correctionlib)
 else()
-    message(STATUS "Setting up correctionlib from local setup ...")
-    set(USING_CVMFS FALSE)
-    find_package(correctionlib REQUIRED PATHS ${CORRECTIONLIBPATH})
-    set(CORRECTION_LIB_PATH "${CORRECTIONLIBPATH}/../lib/libcorrectionlib.so")
+  message(STATUS "Setting up correctionlib from local setup ...")
+  set(USING_CVMFS FALSE)
+  find_package(correctionlib REQUIRED PATHS ${CORRECTIONLIBPATH})
+  set(CORRECTION_LIB_PATH "${CORRECTIONLIBPATH}/../lib/libcorrectionlib.so")
 endif()
 set(THREADS_PREFER_PTHREAD_FLAG ON)
 
--- cmake/Build.cmake
+++ cmake/Build.cmake
@@ -1,52 +1,80 @@
 set(GENERATE_CPP_OUTPUT_FILELIST "${GENERATE_CPP_OUTPUT_DIRECTORY}/files.txt")
 if(NOT EXISTS ${GENERATE_CPP_OUTPUT_FILELIST})
-    message(FATAL_ERROR "List of generated C++ files in ${GENERATE_CPP_OUTPUT_FILELIST} does not exist.")
+  message(
+    FATAL_ERROR
+      "List of generated C++ files in ${GENERATE_CPP_OUTPUT_FILELIST} does not exist."
+  )
 endif()
 
 # Iterate over files from output filelist and add build and install targets
-FILE(READ ${GENERATE_CPP_OUTPUT_FILELIST} FILELIST)
-STRING(REGEX REPLACE "\n" ";" FILELIST ${FILELIST})
+file(READ ${GENERATE_CPP_OUTPUT_FILELIST} FILELIST)
+string(REGEX REPLACE "\n" ";" FILELIST ${FILELIST})
 set(TARGET_NAMES "")
 # copy all correction files into the install location
 install(DIRECTORY data/ DESTINATION ${INSTALLDIR}/data)
-if (PAYLOADS)
-    install(DIRECTORY ${CMAKE_SOURCE_DIR}/analysis_configurations/${ANALYSIS}/payloads DESTINATION ${INSTALLDIR})
+if(PAYLOADS)
+  install(
+    DIRECTORY ${CMAKE_SOURCE_DIR}/analysis_configurations/${ANALYSIS}/payloads
+    DESTINATION ${INSTALLDIR})
 endif()
 
 # also copy inish script needed for job tarball
 install(FILES init.sh DESTINATION ${INSTALLDIR})
 foreach(FILENAME ${FILELIST})
-    # STRING(REGEX REPLACE ".cxx" "" TARGET_NAME ${FILENAME})
-    cmake_path(GET FILENAME RELATIVE_PART RELATIVE_PATH)
-    cmake_path(GET FILENAME FILENAME TARGET_FILENAMENAME)
-    STRING(REGEX REPLACE ".cxx" "" TARGET_NAME ${TARGET_FILENAMENAME})
-    STRING(REGEX REPLACE "/${TARGET_FILENAMENAME}" "" GENERATED_CODEBASE ${RELATIVE_PATH})
+  # STRING(REGEX REPLACE ".cxx" "" TARGET_NAME ${FILENAME})
+  cmake_path(GET FILENAME RELATIVE_PART RELATIVE_PATH)
+  cmake_path(GET FILENAME FILENAME TARGET_FILENAMENAME)
+  string(REGEX REPLACE ".cxx" "" TARGET_NAME ${TARGET_FILENAMENAME})
+  string(REGEX REPLACE "/${TARGET_FILENAMENAME}" "" GENERATED_CODEBASE
+                       ${RELATIVE_PATH})
 
-    list(APPEND TARGET_NAMES ${TARGET_NAME})
-    set(FULL_PATH "${GENERATE_CPP_OUTPUT_DIRECTORY}/${FILENAME}")
+  list(APPEND TARGET_NAMES ${TARGET_NAME})
+  set(FULL_PATH "${GENERATE_CPP_OUTPUT_DIRECTORY}/${FILENAME}")
 
-    # Add build target
-    message(STATUS "Add build target for file ${FILENAME}.")
+  # Add build target
+  message(STATUS "Add build target for file ${FILENAME}.")
 
-    # message(STATUS "FULL_PATH: ${FULL_PATH} / TARGET_NAME: ${TARGET_NAME}")
-    # message(STATUS "Adding header files from ${GENERATE_CPP_OUTPUT_DIRECTORY}/${GENERATED_CODEBASE}/include/*")
-    file(GLOB GENERATED_HEADERS LIST_DIRECTORIES true "${GENERATE_CPP_OUTPUT_DIRECTORY}/${GENERATED_CODEBASE}/include/*")
-    file(GLOB GENERATED_CXX_FILES "${GENERATE_CPP_OUTPUT_DIRECTORY}/${GENERATED_CODEBASE}/src/*/*.cxx")
-    # message(STATUS "GENERATED_HEADERS ${GENERATED_HEADERS}")
-    add_executable(${TARGET_NAME} ${FULL_PATH} ${GENERATED_CXX_FILES})
-    # Adds a pre-build event to the Target copying the correctionlib.so file into the /lib folder in the install directory
-    target_include_directories(${TARGET_NAME} PRIVATE ${CMAKE_SOURCE_DIR} ${ROOT_INCLUDE_DIRS} $ORIGIN/lib/ lib/)
-    target_link_libraries(${TARGET_NAME} ROOT::ROOTVecOps ROOT::ROOTDataFrame ROOT::RooFit ROOT::GenVector logging correctionlib nlohmann_json::nlohmann_json CROWNLIB ${ONNX_RUNTIME_LIB_PATH})
-    add_custom_command(TARGET ${TARGET_NAME} PRE_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different
-        "${CORRECTION_LIB_PATH}"
-        ${INSTALLDIR}/lib/libcorrectionlib.so)
-    # Find shared libraries next to the executable in the /lib folder
-    set_target_properties(${TARGET_NAME} PROPERTIES
-        BUILD_WITH_INSTALL_RPATH FALSE
-        LINK_FLAGS "-Wl,-rpath,$ORIGIN/lib")
-    # Add install target, basically just copying the executable around relative to CMAKE_INSTALL_PREFIX
-    install(TARGETS ${TARGET_NAME} DESTINATION ${INSTALLDIR})
-    install(CODE "execute_process(COMMAND ${CMAKE_SOURCE_DIR}/checks/get-diff.sh ${CMAKE_SOURCE_DIR} ${ANALYSIS} ${INSTALLDIR}/diff )")
+  # message(STATUS "FULL_PATH: ${FULL_PATH} / TARGET_NAME: ${TARGET_NAME}")
+  # message(STATUS "Adding header files from
+  # ${GENERATE_CPP_OUTPUT_DIRECTORY}/${GENERATED_CODEBASE}/include/*")
+  file(
+    GLOB GENERATED_HEADERS
+    LIST_DIRECTORIES true
+    "${GENERATE_CPP_OUTPUT_DIRECTORY}/${GENERATED_CODEBASE}/include/*")
+  file(GLOB GENERATED_CXX_FILES
+       "${GENERATE_CPP_OUTPUT_DIRECTORY}/${GENERATED_CODEBASE}/src/*/*.cxx")
+  # message(STATUS "GENERATED_HEADERS ${GENERATED_HEADERS}")
+  add_executable(${TARGET_NAME} ${FULL_PATH} ${GENERATED_CXX_FILES})
+  # Adds a pre-build event to the Target copying the correctionlib.so file into
+  # the /lib folder in the install directory
+  target_include_directories(
+    ${TARGET_NAME} PRIVATE ${CMAKE_SOURCE_DIR} ${ROOT_INCLUDE_DIRS}
+                           $ORIGIN/lib/ lib/)
+  target_link_libraries(
+    ${TARGET_NAME}
+    ROOT::ROOTVecOps
+    ROOT::ROOTDataFrame
+    ROOT::RooFit
+    ROOT::GenVector
+    logging
+    correctionlib
+    nlohmann_json::nlohmann_json
+    CROWNLIB
+    ${ONNX_RUNTIME_LIB_PATH})
+  add_custom_command(
+    TARGET ${TARGET_NAME}
+    PRE_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different "${CORRECTION_LIB_PATH}"
+            ${INSTALLDIR}/lib/libcorrectionlib.so)
+  # Find shared libraries next to the executable in the /lib folder
+  set_target_properties(
+    ${TARGET_NAME} PROPERTIES BUILD_WITH_INSTALL_RPATH FALSE
+                              LINK_FLAGS "-Wl,-rpath,$ORIGIN/lib")
+  # Add install target, basically just copying the executable around relative to
+  # CMAKE_INSTALL_PREFIX
+  install(TARGETS ${TARGET_NAME} DESTINATION ${INSTALLDIR})
+  install(
+    CODE "execute_process(COMMAND ${CMAKE_SOURCE_DIR}/checks/get-diff.sh ${CMAKE_SOURCE_DIR} ${ANALYSIS} ${INSTALLDIR}/diff )"
+  )
 
 endforeach()
