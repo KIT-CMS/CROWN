22c8644b20ecdcf38616945385430158f2141506
Sebastian Brommer <sebastian.brommer@kit.edu>
2024-03-07 14:48:39 +0100

    add onnxruntime to templates

--- code_generation/analysis_template.cxx
+++ code_generation/analysis_template.cxx
@@ -106,6 +106,9 @@ int main(int argc, char *argv[]) {
     // file logging
     Logger::enableFileLogging("logs/main.txt");
 
+    // start an onnx session manager
+    OnnxSessionManager onnxSessionManager;
+
     // {MULTITHREADING}
 
     // initialize df
