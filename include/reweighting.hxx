a032f6cd0c0e896fa835742969c0a7a981a18358
Sebastian Brommer <sebastian.brommer@kit.edu>
2024-05-22 17:00:12 +0200

    add reweighting functions with CorrectionManager

--- include/reweighting.hxx
+++ include/reweighting.hxx
@@ -6,6 +6,12 @@ ROOT::RDF::RNode puweights(ROOT::RDF::RNode df, const std::string &weightname,
                            const std::string &truePUMean,
                            const std::string &filename,
                            const std::string &histogramname);
+ROOT::RDF::RNode
+puweights(ROOT::RDF::RNode df, CorrectionManager &correctionManager,
+          const std::string &weightname, const std::string &truePU,
+          const std::string &filename, const std::string &eraname,
+          const std::string &variation);
+// Deprecated function without CorrectionManager
 ROOT::RDF::RNode puweights(ROOT::RDF::RNode df, const std::string &weightname,
                            const std::string &truePU,
                            const std::string &filename,
