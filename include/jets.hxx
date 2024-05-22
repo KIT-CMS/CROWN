b16024624460bf25a677d8415fa5eaded2da1982
Sebastian Brommer <sebastian.brommer@kit.edu>
2024-05-22 17:00:28 +0200

    add jets functions with CorrectionManager

--- include/jets.hxx
+++ include/jets.hxx
@@ -33,6 +33,19 @@ ROOT::RDF::RNode CutPUID(ROOT::RDF::RNode df, const std::string &maskname,
                          const std::string &nameID, const std::string &jet_pt,
                          const int &idxID, const float &jet_pt_cut);
 ROOT::RDF::RNode
+JetPtCorrection(ROOT::RDF::RNode df, CorrectionManager &correctionManager,
+                const std::string &corrected_jet_pt, const std::string &jet_pt,
+                const std::string &jet_eta, const std::string &jet_phi,
+                const std::string &jet_area, const std::string &jet_rawFactor,
+                const std::string &jet_ID, const std::string &gen_jet_pt,
+                const std::string &gen_jet_eta, const std::string &gen_jet_phi,
+                const std::string &rho, bool reapplyJES,
+                const std::vector<std::string> &jes_shift_sources,
+                const int &jes_shift, const std::string &jer_shift,
+                const std::string &jec_file, const std::string &jer_tag,
+                const std::string &jes_tag, const std::string &jec_algo);
+// Deprecated function without CorrectionManager
+ROOT::RDF::RNode
 JetPtCorrection(ROOT::RDF::RNode df, const std::string &corrected_jet_pt,
                 const std::string &jet_pt, const std::string &jet_eta,
                 const std::string &jet_phi, const std::string &jet_area,
