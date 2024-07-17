2c6133b5302441fb9a9ac0c69a650a302aeb6653
Moritz Molch <moritz.molch@cern.ch>
2024-07-17 17:28:36 +0200

    Change dynamic to static cast

--- src/hadrecoil.cxx
+++ src/hadrecoil.cxx
@@ -30,7 +30,7 @@ ROOT::RDF::RNode vectorial_mht(ROOT::RDF::RNode df, const std::string &outputnam
         // add up the negative pt vectors of all selected jets 
         auto mht = ROOT::Math::PtEtaPhiMVector(0., 0., 0., 0.);
         for (const int &jet_index : jet_collection) {
-            auto jet_pt_vec = dynamic_cast<ROOT::Math::PtEtaPhiMVector>(ROOT::Math::PtEtaPhiEVector(jet_pt.at(jet_index), jet_eta.at(jet_index), 0., jet_pt.at(jet_index)));
+            auto jet_pt_vec = static_cast<ROOT::Math::PtEtaPhiMVector>(ROOT::Math::PtEtaPhiEVector(jet_pt.at(jet_index), jet_eta.at(jet_index), 0., jet_pt.at(jet_index)));
             mht -= jet_pt_vec;
         }
         return mht;
