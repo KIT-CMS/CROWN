9f846d634f528db307e64c2fe684602c6eb8b9a6
Moritz Molch <moritz.molch@cern.ch>
2024-07-17 16:04:24 +0200

    Add header files for using hadronic recoil functions in producers

--- code_generation/analysis_template.cxx
+++ code_generation/analysis_template.cxx
@@ -4,6 +4,7 @@
 #include "TStopwatch.h"
 #include "include/fatjets.hxx"
 #include "include/genparticles.hxx"
+#include "include/hadrecoil.hxx"
 #include "include/htxs.hxx"
 #include "include/jets.hxx"
 #include "include/lorentzvectors.hxx"
