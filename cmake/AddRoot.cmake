e07e77937baf94c552bbb1a6234c10fa9d29d46d
Sebastian Brommer <sebastian.brommer@kit.edu>
2024-07-23 12:25:15 +0200

    switch to lcg 106, ROOT 6.32 and C++20, drop rhel 8 support

--- cmake/AddRoot.cmake
+++ cmake/AddRoot.cmake
@@ -1,5 +1,4 @@
-find_package(ROOT 6.26 REQUIRED COMPONENTS ROOTVecOps ROOTDataFrame RooFit
-                                           GenVector)
+find_package(ROOT 6.32 REQUIRED COMPONENTS ROOTVecOps ROOTDataFrame RooFit ROOTNTuple GenVector)
 
 message(STATUS "")
 message(STATUS "Found ROOT with following settings:")
@@ -21,23 +20,6 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL GNU)
   message(STATUS "Attach -fconcepts to the compiler flags to silence warnings.")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fconcepts")
 endif()
-
-# Find the C++ standard from ROOT and set it as the standard of this project We
-# require the C++ standard 17 or 20 and don't want to fall back to lower
-# versions.
+# require the C++ standard 20 and don't want to fall back to lower
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
-if(${ROOT_CXX_FLAGS} MATCHES "\\-std\\=c\\+\\+17")
-  message(STATUS "Set c++17 as the C++ standard.")
-  set(CMAKE_CXX_STANDARD 17)
-elseif(${ROOT_CXX_FLAGS} MATCHES "\\-std\\=c\\+\\+20")
-  message(STATUS "Set c++20 as the C++ standard.")
-  set(CMAKE_CXX_STANDARD 20)
-elseif(${ROOT_CXX_FLAGS} MATCHES "\\-std\\=c\\+\\+14")
-  message(STATUS "c++14 found, setting c++17 as the C++ standard.")
-  set(CMAKE_CXX_STANDARD 17)
-else()
-  message(
-    FATAL_ERROR
-      "The standard c++17 or higher is required but not found in the ROOT flags: ${ROOT_CXX_FLAGS}"
-  )
-endif()
+set(CMAKE_CXX_STANDARD 20)
