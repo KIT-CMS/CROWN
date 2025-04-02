0797d96664743e1618a0eedfb91886b710410103
nshadskiy <nikita.shadskiy@kit.edu>
2025-04-02 14:21:02 +0200

    black

--- analysis_configurations/unittest/producers/muons.py
+++ analysis_configurations/unittest/producers/muons.py
@@ -36,7 +36,7 @@ MuonDzCut = Producer(
 )
 MuonIDCut = Producer(
     name="MuonIDCut",
-    call='physicsobject::CutEqual<bool>({df}, {output}, {input}, true)',
+    call="physicsobject::CutEqual<bool>({df}, {output}, {input}, true)",
     input=[nanoAOD.Muon_mediumId],
     output=[],
     scopes=["global"],
@@ -150,7 +150,7 @@ DiMuonVetoPtCut = Producer(
 )
 DiMuonVetoIDCut = Producer(
     name="DiMuonVetoIDCut",
-    call='physicsobject::CutEqual<bool>({df}, {output}, {input}, true)',
+    call="physicsobject::CutEqual<bool>({df}, {output}, {input}, true)",
     input=[nanoAOD.Muon_looseId],
     output=[],
     scopes=["global"],
