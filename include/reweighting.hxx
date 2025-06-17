26a0e153bb77088226e4c1cd83c5be279a66b1a1
nshadskiy <nikita.shadskiy@kit.edu>
2025-06-17 19:49:04 +0200

    reworking reweighting functions and add functions for PS and PDF weights

--- include/reweighting.hxx
+++ include/reweighting.hxx
@@ -1,33 +1,42 @@
 #ifndef GUARD_REWEIGHTING_H
 #define GUARD_REWEIGHTING_H
 
+namespace event {
 namespace reweighting {
+
 ROOT::RDF::RNode
-puweights(ROOT::RDF::RNode df,
-          correctionManager::CorrectionManager &correctionManager,
-          const std::string &weightname, const std::string &truePU,
-          const std::string &filename, const std::string &eraname,
+Pileup(ROOT::RDF::RNode df,
+          correctionManager::CorrectionManager &correction_manager,
+          const std::string &outputname, const std::string &true_pileup_number,
+          const std::string &corr_file, const std::string &corr_name,
           const std::string &variation);
-// Deprecated function without CorrectionManager
-ROOT::RDF::RNode puweights(ROOT::RDF::RNode df, const std::string &weightname,
-                           const std::string &truePU,
-                           const std::string &filename,
-                           const std::string &eraname,
-                           const std::string &variation);
-ROOT::RDF::RNode topptreweighting(ROOT::RDF::RNode df,
-                                  const std::string &weightname,
-                                  const std::string &gen_pdgids,
-                                  const std::string &gen_status,
-                                  const std::string &gen_pt);
-ROOT::RDF::RNode zPtMassReweighting(ROOT::RDF::RNode df,
-                                    const std::string &weightname,
-                                    const std::string &gen_boson,
-                                    const std::string &workspace_file,
-                                    const std::string &functor_name,
-                                    const std::string &argset);
-ROOT::RDF::RNode lhe_scale_weights(ROOT::RDF::RNode df,
-                                   const std::string &weightname,
-                                   const std::string &lhe_scale_weights,
-                                   const float muR, const float muF);
-} // namespace reweighting
+ROOT::RDF::RNode PartonShower(ROOT::RDF::RNode df,
+                            const std::string &outputname,
+                            const std::string &ps_weights,
+                            const float isr, const float fsr);
+ROOT::RDF::RNode LHEscale(ROOT::RDF::RNode df,
+                            const std::string &outputname,
+                            const std::string &lhe_scale_weights,
+                            const float mu_r, const float mu_f);
+ROOT::RDF::RNode LHEpdf(ROOT::RDF::RNode df,
+                        const std::string &outputname,
+                        const std::string &lhe_pdf_weights,
+                        const std::string &variation);
+ROOT::RDF::RNode LHEalphaS(ROOT::RDF::RNode df,
+                        const std::string &outputname,
+                        const std::string &lhe_pdf_weights,
+                        const std::string &variation);
+ROOT::RDF::RNode TopPt(ROOT::RDF::RNode df,
+                        const std::string &outputname,
+                        const std::string &gen_pdg_id,
+                        const std::string &gen_status,
+                        const std::string &gen_pt);
+ROOT::RDF::RNode ZPtMass(ROOT::RDF::RNode df,
+                            const std::string &outputname,
+                            const std::string &gen_boson,
+                            const std::string &workspace_file,
+                            const std::string &functor_name,
+                            const std::string &argset);
+} // end namespace reweighting
+} // end namespace event
 #endif /* GUARD_REWEIGHTING_H */
