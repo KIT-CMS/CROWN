4a8912a84755b933d6ebcad797644f093cbfc583
tvoigtlaender <tim.voigtlaender@kit.edu>
2026-02-24 16:15:09 +0100

    Remove duplicates

--- include/dictionaries/LinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-#pragma link C++ nestedclasses;
-
-#pragma link C++ class std::map<std::string, std::vector<std::string>>+;
-#pragma link C++ class std::map<std::string, std::map<std::string, std::vector<std::string>>>+;
-#endif
