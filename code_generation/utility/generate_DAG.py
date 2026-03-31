0f97db5f88dcae0a84ec5997dabb1e843ebf3ca6
tvoigtlaender <tim.voigtlaender@kit.edu>
2026-03-31 14:42:28 +0200

    Formatting

--- code_generation/utility/generate_DAG.py
+++ code_generation/utility/generate_DAG.py
@@ -2,6 +2,7 @@ import re
 import json
 from code_generation.quantity import QuantityGroup
 
+
 class GraphParser:
     def __init__(self, config, nanoAOD_inputs, debugging=False):
         self.config = config
@@ -42,7 +43,7 @@ class GraphParser:
     def add_node(self, id_name, name=None, scope="global", parent=None, node_type=None):
         if not name:
             name = id_name
-        add_data = {"data":{"id": f"{scope}_{id_name}", "label": name}}
+        add_data = {"data": {"id": f"{scope}_{id_name}", "label": name}}
         if parent:
             add_data["data"]["parent"] = f"{scope}_{parent}"
         if node_type:
@@ -50,10 +51,10 @@ class GraphParser:
         self.nodes += [add_data]
 
     def add_edge(self, source, target, name):
-        self.edges += [{ "data": { "source": source, "target": target, "label": name } }]
+        self.edges += [{"data": {"source": source, "target": target, "label": name}}]
 
     def parse_from_call(self, producer, parent, scope, align=""):
-        group_id =f"{producer.name}_v"
+        group_id = f"{producer.name}_v"
         call = producer.call
         pattern = r'event::filter::Flag\({df}, "{(.*)}", "{(.*)}"\)'
         match = re.search(pattern, call)
@@ -62,12 +63,22 @@ class GraphParser:
         else:
             raise Exception(f"Input vector config could not be parsed from {call}")
         if not input_vec_config in producer.vec_configs:
-            raise Exception(f"Input name from {call} not in producer vector configs {producer.vec_configs}")
+            raise Exception(
+                f"Input name from {call} not in producer vector configs {producer.vec_configs}"
+            )
         else:
             vec_input_index = producer.vec_configs.index(input_vec_config)
 
-        self.add_node(id_name=group_id, name=producer.name, scope=scope, parent=parent, node_type="vector")
-        vec_configs = [self.config.config_parameters[scope][c] for c in producer.vec_configs]
+        self.add_node(
+            id_name=group_id,
+            name=producer.name,
+            scope=scope,
+            parent=parent,
+            node_type="vector",
+        )
+        vec_configs = [
+            self.config.config_parameters[scope][c] for c in producer.vec_configs
+        ]
 
         for i_c, c in enumerate(zip(*vec_configs)):
             input_name = c[vec_input_index]
@@ -75,7 +86,9 @@ class GraphParser:
             vector_name = f"{producer.name}_{i_c}"
             if self.debugging:
                 print(align + "    " + f"Adding Producer: {vector_name}")
-            self.add_node(id_name=vector_id, name=vector_name, scope=scope, parent=group_id)
+            self.add_node(
+                id_name=vector_id, name=vector_name, scope=scope, parent=group_id
+            )
             self.add_input(input_name, vector_id, scope)
             if isinstance(producer.output, list):
                 raise NotImplementedError
@@ -88,27 +101,44 @@ class GraphParser:
             if self.debugging:
                 print()
                 print(f"For scope {scope}:")
-            self.add_node(id_name="scope", name=f"{scope} scope", scope=scope, node_type="scope")
+            self.add_node(
+                id_name="scope", name=f"{scope} scope", scope=scope, node_type="scope"
+            )
             for p in self.config.producers[scope]:
-                self.parse_producer(producer=p, parent=f"scope", scope=scope, align="    ")
+                self.parse_producer(
+                    producer=p, parent="scope", scope=scope, align="    "
+                )
             for output in self.config.outputs[scope]:
                 if isinstance(output, QuantityGroup):
                     vec_config = self.config.config_parameters[scope][output.vec_config]
                     vec_output_name = self.vec_output_mappings[output.name]
                     for o in vec_config:
-                        self.add_input(o[vec_output_name], f"{self.out_file_name}", scope)
+                        self.add_input(
+                            o[vec_output_name], f"{self.out_file_name}", scope
+                        )
                 else:
                     self.add_input(output.name, f"{self.out_file_name}", scope)
-            self.add_node(id_name=self.out_file_name, name=f"{scope}_{self.out_file_name}", scope=scope, node_type="I/O")
+            self.add_node(
+                id_name=self.out_file_name,
+                name=f"{scope}_{self.out_file_name}",
+                scope=scope,
+                node_type="I/O",
+            )
 
         # raise NotImplementedError
-        #check outputs for ambigous assignment
+        # check outputs for ambigous assignment
         for scope in self.config.scopes:
-            all_keys = list(self.outputs["global"].keys()) + list(self.outputs[scope].keys())
+            all_keys = list(self.outputs["global"].keys()) + list(
+                self.outputs[scope].keys()
+            )
             for key in all_keys:
-                total_output_nodes = self.outputs["global"].get(key, []) + self.outputs[scope].get(key, [])
+                total_output_nodes = self.outputs["global"].get(key, []) + self.outputs[
+                    scope
+                ].get(key, [])
                 if len(set(total_output_nodes)) != 1:
-                    raise Exception(f"Output {output} has multiple origins: {total_output_nodes}")
+                    raise Exception(
+                        f"Output {output} has multiple origins: {total_output_nodes}"
+                    )
         self.assemble_edges()
         self.DAG_data = self.nodes + self.edges
 
@@ -134,84 +164,116 @@ class GraphParser:
                         print(f"Adding Edge {composite_name} from {o} to {k}")
                     self.add_edge(source=o, target=k, name=composite_name)
 
-    def parse_producer(self, producer, parent, scope, align=""):
-        if producer.__class__.__name__ in ["VectorProducer"]:
-            if self.debugging:
-                print(align + f"Adding VectorProducer: {producer.name}")
-            print(f"!!!{producer} is a lagacy producer and should be replaced with ExtendedVectorProducer!!!")
-            if producer.call.startswith('event::filter::Flag'):
-                self.parse_from_call(producer=producer, parent=parent, scope=scope, align=align)
-            else:
-                raise NotImplementedError(f"The call {producer.call} does not have legacy support.")
+    def parse_VectorProducer(self, producer, parent, scope, align=""):
+        if self.debugging:
+            print(align + f"Adding VectorProducer: {producer.name}")
+        print(
+            f"!!!{producer} is a lagacy producer and should be replaced with ExtendedVectorProducer!!!"
+        )
+        if producer.call.startswith("event::filter::Flag"):
+            self.parse_from_call(
+                producer=producer, parent=parent, scope=scope, align=align
+            )
+        else:
+            raise NotImplementedError(
+                f"The call {producer.call} does not have legacy support."
+            )
 
-        elif producer.__class__.__name__ in ["ExtendedVectorProducer"]:
+    def parse_ExtendedVectorProducer(self, producer, parent, scope, align=""):
+        if self.debugging:
+            print(align + f"Adding VectorProducer: {producer.name}")
+        group_id = f"{producer.name}_v"
+        self.add_node(
+            id_name=group_id,
+            name=producer.name,
+            scope=scope,
+            parent=parent,
+            node_type="vector",
+        )
+        vec_config = self.config.config_parameters[scope][producer.vec_config]
+        for i_c, c in enumerate(vec_config):
+            vector_id = f"{group_id}_{i_c}"
+            vector_name = f"{producer.name}_{i_c}"
             if self.debugging:
-                print(align + f"Adding VectorProducer: {producer.name}")
-            group_id =f"{producer.name}_v"
-            self.add_node(id_name=group_id, name=producer.name, scope=scope, parent=parent, node_type="vector")
-            vec_config = self.config.config_parameters[scope][producer.vec_config]
-            for i_c, c in enumerate(vec_config):
-                vector_id = f"{group_id}_{i_c}"
-                vector_name = f"{producer.name}_{i_c}"
-                if self.debugging:
-                    print(align + "    " + f"Adding Producer: {vector_name}")
-                self.add_node(id_name=vector_id, name=vector_name, scope=scope, parent=group_id)
-                if isinstance(producer.input[scope], list):
-                    for n in producer.input[scope]:
-                        self.add_input(n.name, vector_id, scope)
-                        if self.debugging:
-                            print(align + "        " + f"Adding Input: {n.name}")
-                if isinstance(producer.output, list):
-                    vec_output = c[producer.outputname]
-                    self.vec_output_mappings[producer.output_group.name] = producer.outputname
-                    self.add_output(vec_output, vector_id, scope)
+                print(align + "    " + f"Adding Producer: {vector_name}")
+            self.add_node(
+                id_name=vector_id, name=vector_name, scope=scope, parent=group_id
+            )
+            if isinstance(producer.input[scope], list):
+                for n in producer.input[scope]:
+                    self.add_input(n.name, vector_id, scope)
                     if self.debugging:
-                        print(align + "        " + f"Adding Output: {vec_output}")
+                        print(align + "        " + f"Adding Input: {n.name}")
+            if isinstance(producer.output, list):
+                vec_output = c[producer.outputname]
+                self.vec_output_mappings[producer.output_group.name] = (
+                    producer.outputname
+                )
+                self.add_output(vec_output, vector_id, scope)
+                if self.debugging:
+                    print(align + "        " + f"Adding Output: {vec_output}")
 
-        elif producer.__class__.__name__ == "ProducerGroup":
-            if self.debugging:
-                print(align + f"Adding ProducerGroup: {producer.name}")
-            group_id =f"{producer.name}_g"
-            self.add_node(id_name=group_id, name=producer.name, scope=scope, parent=parent)
-            for p in producer.producers[scope]:
-                self.parse_producer(producer=p, parent=group_id, scope=scope, align=align + "    ")
+    def parse_ProducerGroup(self, producer, parent, scope, align=""):
+        if self.debugging:
+            print(align + f"Adding ProducerGroup: {producer.name}")
+        group_id = f"{producer.name}_g"
+        self.add_node(id_name=group_id, name=producer.name, scope=scope, parent=parent)
+        for p in producer.producers[scope]:
+            self.parse_producer(
+                producer=p, parent=group_id, scope=scope, align=align + "    "
+            )
 
-        elif producer.__class__.__name__ in ["Filter"]:
-            if self.debugging:
-                print(align + f"Adding Filter: {producer.name}")
-            group_id =f"{producer.name}_g"
-            self.add_node(id_name=group_id, name=producer.name, scope=scope, parent=parent)
-            for p in producer.producers[scope]:
-                self.parse_producer(producer=p, parent=group_id, scope=scope, align=align + "    ")
+    def parse_Filter(self, producer, parent, scope, align=""):
+        if self.debugging:
+            print(align + f"Adding Filter: {producer.name}")
+        group_id = f"{producer.name}_g"
+        self.add_node(id_name=group_id, name=producer.name, scope=scope, parent=parent)
+        for p in producer.producers[scope]:
+            self.parse_producer(
+                producer=p, parent=group_id, scope=scope, align=align + "    "
+            )
 
-        elif producer.__class__.__name__ in ["BaseFilter"]:
+    def parse_BaseFilter(self, producer, parent, scope, align=""):
+        if self.debugging:
+            print(align + f"Adding Filter: {producer.name}")
+        self.add_node(id_name=producer.name, scope=scope, parent=parent)
+        for n in producer.input[scope]:
+            self.add_input(n.name, producer.name, scope)
             if self.debugging:
-                print(align + f"Adding Filter: {producer.name}")
-            self.add_node(id_name=producer.name, scope=scope, parent=parent)
+                print(align + "    " + f"Adding Input: {n.name}")
+
+    def parse_Producer(self, producer, parent, scope, align=""):
+        if self.debugging:
+            print(align + f"Adding Producer: {producer.name}")
+        self.add_node(id_name=producer.name, scope=scope, parent=parent)
+        if isinstance(producer.input[scope], list):
             for n in producer.input[scope]:
                 self.add_input(n.name, producer.name, scope)
                 if self.debugging:
                     print(align + "    " + f"Adding Input: {n.name}")
+        if isinstance(producer.output, list):
+            for n in producer.output:
+                self.add_output(n.name, producer.name, scope)
+                if self.debugging:
+                    print(align + "    " + f"Adding Output: {n.name}")
 
+    def parse_producer(self, producer, parent, scope, align=""):
+        if producer.__class__.__name__ == "VectorProducer":
+            self.parse_VectorProducer(producer, parent, scope, align)
+        elif producer.__class__.__name__ == "ExtendedVectorProducer":
+            self.parse_ExtendedVectorProducer(producer, parent, scope, align)
+        elif producer.__class__.__name__ == "ProducerGroup":
+            self.parse_ProducerGroup(producer, parent, scope, align)
+        elif producer.__class__.__name__ == "Filter":
+            self.parse_Filter(producer, parent, scope, align)
+        elif producer.__class__.__name__ == "BaseFilter":
+            self.parse_BaseFilter(producer, parent, scope, align)
         elif producer.__class__.__name__ == "Producer":
-            if self.debugging:
-                print(align + f"Adding Producer: {producer.name}")
-            self.add_node(id_name=producer.name, scope=scope, parent=parent)
-            if isinstance(producer.input[scope], list):
-                for n in producer.input[scope]:
-                    self.add_input(n.name, producer.name, scope)
-                    if self.debugging:
-                        print(align + "    " + f"Adding Input: {n.name}")
-
-            if isinstance(producer.output, list):
-                for n in producer.output:
-                    self.add_output(n.name, producer.name, scope)
-                    if self.debugging:
-                        print(align + "    " + f"Adding Output: {n.name}")
-
+            self.parse_Producer(producer, parent, scope, align)
         else:
             print(producer)
             raise Exception(f"Unknown Producer class {producer.__class__.__name__}")
+
 
 def create_graph(configuration, nanoAOD_inputs, json_name):
     graph = GraphParser(configuration, nanoAOD_inputs, debugging=False)
